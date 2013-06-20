285d2f6d739e3e14e9db19df18bdf43b3e8b47cf
Patrick <patrick@betterment.com>
2013-06-20 12:14:08 -0400

    tidied code

--- dbcon.R
+++ dbcon.R
@@ -1,9 +1,9 @@
 require(RMySQL);
 
 database.name <- 'bettermentdb'  			# name of the database file you're using
-dbcon       	<- dbConnect(dbDriver('MySQL'), dbname=database.name, user='root', host='127.0.0.1')
+dbcon         <- dbConnect(dbDriver('MySQL'), dbname=database.name, user='root', host='127.0.0.1')
 
-# # now you can use SQL queries to fetch data from the table
+#  now you can use SQL queries to fetch data from the table
 # query <- "SELECT * from Users u where u.howHeard = 'Friend'"
 # x     <- dbGetQuery(conn = dbcon, query)
 # head(x)
